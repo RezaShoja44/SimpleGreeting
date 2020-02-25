b649774d1c3c14f732539dc9c76c03d8807e6734
RezaShoja <rezasj88@gmail.com>
2020-02-26 00:35:15 +0330

    simple greeting with user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import ReactDOM from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let element;
+  const user = {
+    firstName: prompt("Your First Name?", "Reza"),
+    lastName: prompt("Your Last Name?", "Shoja"),
+    age: prompt("Your Age?", "18")
+  };
+
+  function userGreeting(person) {
+    element =
+      person.age > 18 ? person.firstName + " " + person.lastName : "Stranger";
+  }
+  element = <h1 className="App">Hello, {userGreeting(user) || element}</h1>;
+  return ReactDOM.render(element, document.getElementById("root"));
 }
 
 export default App;
